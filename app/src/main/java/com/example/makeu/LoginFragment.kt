546f6b81121cbf265ea5ce0aa51fa05ae026b850
aabiyah <aabiyah@gmail.com>
2023-02-24 21:14:29 +0530

    Exception handling for login/signup pages

--- app/src/main/java/com/example/makeu/LoginFragment.kt
+++ app/src/main/java/com/example/makeu/LoginFragment.kt
@@ -1,34 +1,20 @@
 package com.example.makeu
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var username: EditText
+    private lateinit var password: EditText
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,30 +22,38 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        username = view.findViewById(R.id.login_username)
+        password = view.findViewById(R.id.login_password)
+
         view.findViewById<Button>(R.id.button_signup).setOnClickListener {
             var navRegister = activity as FragmentNavigation
             navRegister.navigateFrag(SignUpFragment(), false)
         }
+
+        view.findViewById<Button>(R.id.button_login).setOnClickListener {
+            validateForm()
+        }
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun validateForm() {
+        when {
+            TextUtils.isEmpty(username.text.toString().trim()) -> {
+                username.error = "Please Enter Username"
+            }
+            TextUtils.isEmpty(password.text.toString().trim()) -> {
+                password.error = "Please Enter Password"
+            }
+
+            username.text.toString().isNotEmpty() &&
+                    password.text.toString().isNotEmpty()  -> {
+                if(username.text.toString().matches(Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"))) {
+                    Toast.makeText(context, "Login Successful!", Toast.LENGTH_SHORT).show()
+                } else {
+                    username.error = "Please Enter Valid Email ID"
                 }
             }
+        }
     }
 }
--- app/src/main/java/com/example/makeu/SignUpFragment.kt
+++ app/src/main/java/com/example/makeu/SignUpFragment.kt
@@ -1,34 +1,22 @@
 package com.example.makeu
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var confirmPassword: EditText
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,30 +25,48 @@ class SignUpFragment : Fragment() {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_sign_up, container, false)
 
+        username = view.findViewById(R.id.reg_username)
+        password = view.findViewById(R.id.reg_password)
+        confirmPassword = view.findViewById(R.id.reg_confirm_password)
+
         view.findViewById<Button>(R.id.button_login_reg).setOnClickListener {
             var navRegister = activity as FragmentNavigation
             navRegister.navigateFrag(LoginFragment(), false)
         }
+        view.findViewById<Button>(R.id.button_signup_reg).setOnClickListener {
+            validateEmptyForm()
+        }
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun validateEmptyForm() {
+        when {
+            TextUtils.isEmpty(username.text.toString().trim()) -> {
+                username.setError("Please Enter Username")
             }
+            TextUtils.isEmpty(password.text.toString().trim()) -> {
+                password.setError("Please Enter Password")
+            }
+            TextUtils.isEmpty(confirmPassword.text.toString().trim()) -> {
+                confirmPassword.setError("Passwords Don't Match!")
+            }
+            username.text.toString().isNotEmpty() &&
+                    password.text.toString().isNotEmpty() &&
+                    confirmPassword.text.toString().isNotEmpty() -> {
+                        if(username.text.toString().matches(Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"))) {
+                            if(password.text.toString().length>=5) {
+                                if(password.text.toString() == confirmPassword.text.toString()) {
+                                    Toast.makeText(context, "Sign Up Successful!", Toast.LENGTH_SHORT).show()
+                                } else {
+                                    confirmPassword.setError("Passwords Don't Match!")
+                                }
+                            } else {
+                                password.setError("Please Enter At Least 5 Characters")
+                            }
+                        } else {
+                            username.setError("Please Enter Valid Email ID")
+                        }
+                    }
+        }
     }
 }
